467e96a0aeaf632f4262bcbf9be06406b25055c4
Thomas Bance-Laupenie <thomas170303@gmail.com>
2023-03-01 23:24:21 +0100

    quasi fin TP

--- TP2Navire/Navire.cs
+++ TP2Navire/Navire.cs
@@ -1,39 +1,99 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using GestionNavire.Exceptions;
 
-namespace GestionNavire.Classesmetier
+namespace Navire.Classesmetier
 {
     class Navire
     {
-        private readonly string imo;
-        private readonly string nom;
+        private string imo;
+        private string nom;
         private string libelleFret;
         private int qteFretMaxi;
+        private int qteFret;
 
-        public Navire(string imo, string nom, string libelleFret, int qteFretMaxi)
+
+        public Navire(string imo, string nom, string libelleFret, int qteFretMaxi, int qteFret)
         {
-            this.imo = imo;
+            this.Imo = imo;
             this.nom = nom;
             this.libelleFret = libelleFret;
             this.QteFretMaxi = qteFretMaxi;
+            this.qteFret = qteFret;
         }
-        public Navire(string imo, string nom) : this(imo, nom, "Indéfini", 0) { }
+        public Navire(string imo, string nom) : this(imo, nom, "Indéfini", 0, 0) { }
 
-        public string Imo => imo;
+
+        public int QteFret
+        {
+            get => qteFret;
+            set
+            {
+                if (value >= 0 & value < qteFretMaxi)
+                {
+                    this.qteFret = value;
+                }
+                else
+                {
+                    throw new GestionPortException("Valeur incohérente pour la quantité de fret stockée dans le navire");
+                }
+            }
+        }
+
+        public string Imo
+        {
+            get => imo;
+            set
+            {
+                if (Regex.IsMatch(value, @"^IMO[0-9]{7}$"))
+                {
+                    this.imo = value;
+                }
+                else
+                {
+                    throw new GestionPortException("erreur : numéro IMO non valide");
+                }
+            }
+        }
 
         public string Nom => nom;
         public string LibelleFret { get => libelleFret; set => libelleFret = value; }
-        public int QteFretMaxi { get => qteFretMaxi; set => qteFretMaxi = value; }
-
-        public override String ToString()
+        public int QteFretMaxi
         {
-            String Message = "Identification \t: " + this.imo;
-            Message += "\nNom :\t" + this.nom;
-            Message += "\nType de frêt :\t" + this.libelleFret;
-            Message += "\nQuantité de Frêt : \t" + this.qteFretMaxi;
-            Message += "\n-------------------------------\n";
-            return Message;
+            get => qteFretMaxi;
+            set
+            {
+                if (value >= 0)
+                {
+                    this.qteFretMaxi = value;
+                }
+                else
+                {
+                    throw new GestionPortException("Erreur, quantité de fret non valide");
+                }
+            }
+        }
+
+        public void Decharger(int quantite)
+        {
+            if (quantite < 0)
+            {
+                throw new GestionPortException("La quantité à décharger ne peut être négative ou nulle");
+            }
+            else if (quantite > this.QteFret)
+            {
+                throw new GestionPortException("Impossible de décharger plus que la quantité de fret dans le navire");
+            }
+            this.QteFret -= quantite;
+        }
+
+        public bool EstDecharge()
+        {
+            return this.QteFret == 0;
         }
     }
 }
--- TP2Navire/Port.cs
+++ TP2Navire/Port.cs
@@ -1,69 +1,139 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GestionNavire.Exceptions;
+using GestionNavire.ClassesMetier;
 
-namespace GestionNavire.Classesmetier
+namespace Navire.Classesmetier
 {
     class Port
     {
-
-        private readonly string nom;
-        private int nbNaviresMax = 5;
+        private string nom;
+        private int nbNaviresMax;
         private Dictionary<string, Navire> navires;
-        
-        //constructeur
+        private List<Stockage> stockages;
+
         public Port(string nom)
         {
             this.nom = nom;
-            navires = new Dictionary<string, Navire>();
+            this.nbNaviresMax = 5;
+            this.navires = new Dictionary<string, Navire>();
+
         }
 
-        // Methods
+        internal Dictionary<string, Navire> Navires { get => navires; set => navires = value; }
+
+
+
         public void EnregistrerArrivee(Navire navire)
         {
-            if (this.navires.Count < this.nbNaviresMax)
+            try
             {
-                this.navires.Add(navire.Imo,navire);
+                if (this.navires.Count < this.nbNaviresMax)
+                {
+                    this.navires.Add(navire.Imo, navire);
+                }
+                else
+                {
+                    throw new GestionPortException("Ajout impossible, le port est complet");
+                }
             }
-            else
+            catch (ArgumentException)
             {
-                throw new Exception("Ajout impossible, le port est complet");
+                throw new GestionPortException("Le navire " + navire.Imo + " est déjà enregistré");
             }
-        }
 
-
-        public void EnregistrerDepart(String imo)
-        {
-            if (this.EstPresent(imo))
-            {
-                // le navire est présent dans le port
-                this.navires.Remove(imo);
-            }
-            else
-            {
-                // le navire n'est pas dans le port
-                throw new Exception("impossible d'enregistrer le départ du navire " + imo + ", il n'est pas dans le port ");
-            }
         }
 
         public bool EstPresent(String imo)
         {
-            return this.navires.ContainsKey(imo);
+            return navires.ContainsKey(imo);
         }
 
-        public int QteFretMaxi
+        public bool EstPresent(Navire navire)
         {
-            get => QteFretMaxi;
-            set
+            return navires.ContainsKey(navire.Imo);
+        }
+
+        public void EnregistrerDepart(String imo)
+        {
+            if (EstPresent(imo))
             {
-                if (value >= 0)
+                this.navires.Remove(imo);
+            }
+            else
+            {
+                throw new GestionPortException("Impossible d'enregistrer le départ du navire " + imo + ", il n'est pas dans le port");
+            }
+        }
+
+        public void Dechargement(String imo)
+        {
+            Navire navire = GetNavire(imo);
+            if (navire != null && navire.LibelleFret == "Porte-conteneur")
+            {
+                int i = 0;
+                while (i < stockages.Count() && !navire.EstDecharge())
                 {
-                    this.QteFretMaxi = value;
+                    //On est sur un stockage
+                    // On tes d'abord si le stockage disponible est > 0
+                    // 2 possibilités : Soit décharger l'entierté du bateau soit partiellement
+                    if (this.stockages[i].CapaciteDispo > 0)
+                    {
+                        // On va pouvoir décharger
+                        if (navire.QteFret <= stockages[i].CapaciteDispo)
+                        {
+                            // On va décharger la totalité du navire
+                            stockages[i].Stocker(navire.QteFret);
+                            navire.Decharger(navire.QteFret);
+                        }
+                        else
+                        {
+                            //On décharge partiellement
+                            navire.Decharger(stockages[i].CapaciteDispo);
+                            stockages[i].Stocker(stockages[i].CapaciteDispo);
+
+                        }
+                    }
+                    i++;
                 }
-                else
+                // On sort de la boucle et on test comment on est sorti de la boucle
+                // On déclenchera une exception si le navire n'est pas complètement déchargé
+
+                if (!navire.EstDecharge())
                 {
-                    throw new Exception("Erreur, quantité de fret non valide");
+                    throw new GestionPortException("Le navire n'a pas pu être entièrement déchargé");
                 }
             }
+            else
+            {
+                throw new GestionPortException("Le navire ne peut pas être déchargé");
+            }
+        }
+
+        public void AjoutStockage(Stockage stockage)
+        {
+            stockages.Add(stockage);
+        }
+
+        public Navire GetNavire(String imo)
+        {
+            if (navires.ContainsKey(imo))
+            {
+                return navires[imo];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        Navire n;
+        public bool GetNavire(String imo, out Navire n)
+        {
+            return navires.TryGetValue(imo, out n);
         }
     }
 }
